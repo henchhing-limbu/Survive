9300231a3c11db54a47742cc1d0ac147cce6086c
Tabbay <39598442+Tabbay@users.noreply.github.com>
2018-11-28 00:14:10 -0800

    Update Jungle.py

--- Jungle.py
+++ Jungle.py
@@ -1,4 +1,4 @@
-class State:
+class State2:
   def __init__(self, scene='', optns = {}, final=False, dead=False):
     self.scene = scene          # this explains the situation
     self.finalState = final   # boolean saying if the state is final
@@ -94,15 +94,15 @@ def makeaChoice(option, transit, curr_state):
 # from State.py import State
 
 def JungeScene(player):
-    JungleS4 = State(' You avoid this man and continue to explore the temple area on your own. You find some materials that look like they can be used to build a boat.',{},True,False)
-    JungleS6 = State('The man leads you through the temple to the explorers’ campsite. Luck for you, you catch them before they leave the island and they agreed to take you with them. You win!',{},True,False)
-    JungleS2 = State(' A predator in the distance causes all the animals to flee in fear. A stampede is now coming your way and you cannot out run it. You die.',{},False,True)
-    JungleS1= State('You see a herd of animals by a river. ', {'E': JungleS2}, False,False)
-    JungleS7 = State('You begin to get angry and the man can tell. Your anger scares the old man and he now believes you are a threat. He calls his friends and they take you captive and you are never seen again. You lose!', {},False,True)
-    JungleS5 = State('You speak to the man and he believes he has seen you before. The man mistakes you for  one of the explorers he guided around the temple a few days ago. Despite your efforts, the man still believes that you are one of the explorers.',{'A': JungleS6, 'B': JungleS7},False,False)
-    JungleS3 = State(' You think following the tracks of a mysterious animal is a bad idea so you continue on in the opposite direction of the tracks. You find yourself stumbling upon the ruins of an ancient temple. As you approach the entrance of the temple, far off in the distance you see an old man who hasn’t noticed your presence yet.',{'A': JungleS4,'B': JungleS5},False,False)
+    JungleS4 = State2(' You avoid this man and continue to explore the temple area on your own. You find some materials that look like they can be used to build a boat.',{},True,False)
+    JungleS6 = State2('The man leads you through the temple to the explorers’ campsite. Luck for you, you catch them before they leave the island and they agreed to take you with them. You win!',{},True,False)
+    JungleS2 = State2(' A predator in the distance causes all the animals to flee in fear. A stampede is now coming your way and you cannot out run it. You die.',{},False,True)
+    JungleS1= State2('You see a herd of animals by a river. ', {'E': JungleS2}, False,False)
+    JungleS7 = State2('You begin to get angry and the man can tell. Your anger scares the old man and he now believes you are a threat. He calls his friends and they take you captive and you are never seen again. You lose!', {},False,True)
+    JungleS5 = State2('You speak to the man and he believes he has seen you before. The man mistakes you for  one of the explorers he guided around the temple a few days ago. Despite your efforts, the man still believes that you are one of the explorers.',{'A': JungleS6, 'B': JungleS7},False,False)
+    JungleS3 = State2(' You think following the tracks of a mysterious animal is a bad idea so you continue on in the opposite direction of the tracks. You find yourself stumbling upon the ruins of an ancient temple. As you approach the entrance of the temple, far off in the distance you see an old man who hasn’t noticed your presence yet.',{'A': JungleS4,'B': JungleS5},False,False)
 
-    JungleS0 = State('You go off to explore more of the island. After wandering miles in the island heat you see a jungle in the distance. You enter into the shade of the jungle to escape the smoldering heat and you happen to look down towards your feet. You notice that there are some type of animal tracks on the ground.',{'A': JungleS1,'B': JungleS3},False,False)
+    JungleS0 = State2('You go off to explore more of the island. After wandering miles in the island heat you see a jungle in the distance. You enter into the shade of the jungle to escape the smoldering heat and you happen to look down towards your feet. You notice that there are some type of animal tracks on the ground.',{'A': JungleS1,'B': JungleS3},False,False)
 
 
 
